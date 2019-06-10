4eeced61c959583986613be8bb021db2c2acf0b8
BrandonJoe <brandon.joe42@yahoo.com>
2019-06-09 19:09:25 -0700

    Updates

---
+++ precache-manifest.efb178e38f4ad01d913cc16a76cec5ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ca07d178b228012dab8949200dd0ff",
+    "url": "/CorvetteQuiz/index.html"
+  },
+  {
+    "revision": "6259994338f4587a5ea1",
+    "url": "/CorvetteQuiz/static/css/main.59b38c7c.chunk.css"
+  },
+  {
+    "revision": "627d516e706b6da82879",
+    "url": "/CorvetteQuiz/static/js/2.b4a18b25.chunk.js"
+  },
+  {
+    "revision": "6259994338f4587a5ea1",
+    "url": "/CorvetteQuiz/static/js/main.3eb93491.chunk.js"
+  },
+  {
+    "revision": "ce13205c0ddd677c311f",
+    "url": "/CorvetteQuiz/static/js/runtime~main.357c9d19.js"
+  }
+]);
